f551dbf57d22780be2d13ba87fc6aa9a0b213fea
seif-alhorani <horanisaif5@gmail.com>
2026-03-25 11:28:33 +0300

    Creating All Blogs page

--- script/script-blogs.js
+++ script/script-blogs.js
@@ -1,22 +1,22 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    const blog_container = document.getElementById("blog-container");
+document.addEventListener("DOMContentLoaded",function(){
+    const blogContainer = document.getElementById("all-blogs-container");
     const storedBlogs = localStorage.getItem("blogs");
-
     let blogs = storedBlogs ? JSON.parse(storedBlogs) : [];
-    if (blogs.length === 0) {
-        blog_container.innerHTML = `<h2 style="text-align: center; color: var(--text-muted-light);"> No blogs are avilable here be the first to create one </h2>`
+
+    if(blogs.length === 0){
+        blogContainer.innerHTML=` <h2 style="color: var(--text-muted-light); grid-column: 1/-1; text-align: center;">No blogs found. Create some!</h2>`
     }else{
+        let allBlogs = [...blogs].reverse();
         function stringlimitcheck(text,limit){
             if(text.length > limit){
                 return text.substring(0,limit);
             }
             return text;
         }
-        let latestblog = [...blogs].reverse().slice(0, 4);
-        let Content = "";
-        latestblog.forEach(element => {
-            Content += `<div class="blog-card">
+        
+        let htmlContent ="";
+        allBlogs.forEach(element =>{
+            htmlContent+=`<div class="blog-card">
                 <div class="card-image">
                     <img src="${element.image}" alt="">
                     <span class="category-label">${element.category}</span>
@@ -28,9 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </div>`;
         });
-        blog_container.innerHTML = Content;
+        blogContainer.innerHTML=htmlContent;
     }
-
 });
- //problem with uploading img  function on create blog need to be fixed 
-
