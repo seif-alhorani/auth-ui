19970fa18801e2437fe4f800134b6ef13a3d775a
seif-alhorani <horanisaif5@gmail.com>
2026-03-30 12:42:19 +0300

    Support API Fetch

--- script/script-home.js
+++ script/script-home.js
@@ -1,10 +1,37 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+async function getData() {
+    const url = "https://newsapi.org/v2/everything?q=tesla&from=2026-02-28&sortBy=publishedAt&apiKey=Your API KEY Here";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result.articles;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+
     const blog_container = document.getElementById("blog-container");
     const storedBlogs = localStorage.getItem("blogs");
-
     let blogs = storedBlogs ? JSON.parse(storedBlogs) : [];
-    if (blogs.length === 0) {
+
+    let apiblog=await getData();
+
+    let apiblogmap =apiblog.map(data=>({
+        title:data.title,
+        image:data.urlToImage,
+        category:"News",    
+        content:data.content,
+        createdAt:JSON.stringify(data.publishedAt)
+    }));
+     
+    let allblogs = [...apiblogmap];
+    if (allblogs.length === 0) {
         blog_container.innerHTML = `<h2 style="text-align: center; color: var(--text-muted-light);"> No blogs are avilable here be the first to create one </h2>`
     }else{
         function stringlimitcheck(text,limit){
@@ -13,7 +40,8 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return text;
         }
-        let latestblog = [...blogs].reverse().slice(0, 4);
+        let latestblog = allblogs.reverse().slice(0, 4);
+    
         let Content = "";
         latestblog.forEach(element => {
             Content += `<div class="blog-card"  onclick="location.href='single-blog.html?title=${element.title}'">
